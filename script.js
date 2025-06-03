9f7b10d6d930b74287d1ed3e8c0f2286c843da12
Queren Sales <querenhsales@gmail.com>
2025-06-03 14:53:00 -0300

    criando: botao concluir

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const criarTarefa = (evento) => {
     tarefa.classList.add('task')
     const conteudo = `<p class="content">${valor}</p>`
     tarefa.innerHTML = conteudo
-
+    tarefa.appendChild(BotaoConcluir())
     list.appendChild(tarefa)
 
     input.value = ""
@@ -17,3 +17,18 @@ const criarTarefa = (evento) => {
 
 const novaTarefa = document.querySelector('[data-form-button]');
 novaTarefa.addEventListener('click', criarTarefa)
+
+const BotaoConcluir = () => {
+    const BotaoConcluir = document.createElement('button')
+    BotaoConcluir.classList.add('check-button')
+    BotaoConcluir.innerText = 'Concluir'
+    BotaoConcluir.addEventListener('click', concluirTarefa)
+
+    return BotaoConcluir
+}
+
+const concluirTarefa = (evento) => {
+    const BotaoConcluir = evento.target
+    const tarefaCompleta = BotaoConcluir.parentElement
+    tarefaCompleta.classList.toggle('done')
+}
